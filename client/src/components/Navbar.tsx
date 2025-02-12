dddb1458baad2c0824e966a0075151caedaf60fe
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-02-12 19:39:27 +0530

    Added Mobile Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -13,8 +13,32 @@ import {
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import { Button } from "./ui/button";
-import { Loader2, Moon, ShoppingCart, Sun } from "lucide-react";
+import {
+  HandPlatter,
+  Loader2,
+  Menu,
+  Moon,
+  PackageCheck,
+  ShoppingCart,
+  SquareMenu,
+  Sun,
+  User,
+  UtensilsCrossed,
+} from "lucide-react";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Label } from "./ui/label";
+import { Input } from "./ui/input";
+import { Separator } from "./ui/separator";
 
 export const Navabar = () => {
   const admin = false;
@@ -25,7 +49,7 @@ export const Navabar = () => {
         <Link to="/">
           <h1 className="text-2xl font-bold">Eat&Dine</h1>{" "}
         </Link>
-        <div className="flex items-center gap-10">
+        <div className="hidden md:flex items-center gap-10">
           <div className="flex items-center gap-6">
             <Link to="/">Home</Link>
             <Link to="/profile">Profile</Link>
@@ -97,9 +121,108 @@ export const Navabar = () => {
               )}
             </div>
           </div>
-          <div>{/* Mobile Resoponsive */}</div>
+        </div>
+        <div className="md:hidden">
+          {/* Mobile Responsive */}
+          <MobileNavbar />
         </div>
       </div>
     </div>
+  );
+};
+
+const MobileNavbar = () => {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">
+          <Menu
+            size={"icon"}
+            className="rounded-full text-black bg-gray-200 hover:bg-gray-300"
+          />
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">
+        <SheetHeader className="flex flex-row items-center justify-between">
+          <SheetTitle>Eat&Dine</SheetTitle>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" size="icon">
+                <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span className="sr-only">Toggle theme</span>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuItem>Light</DropdownMenuItem>
+              <DropdownMenuItem>Dark</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </SheetHeader>
+        <Separator className="my-2" />
+        <SheetDescription className="flex-1">
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <User />
+            <span>Profile</span>
+          </Link>
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <HandPlatter />
+            <span>Order</span>
+          </Link>
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <ShoppingCart />
+            <span>Cart (0)</span>
+          </Link>
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <SquareMenu />
+            <span>Menu</span>
+          </Link>
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <UtensilsCrossed />
+            <span>Resturant</span>
+          </Link>
+          <Link
+            to="/profile"
+            className="flex items-center gap-4 hover:bg-gray-200 px-3 py-2 rounded-lg cursor-pointer hover:text-gray-900 font-medium"
+          >
+            <PackageCheck />
+            <span>Resturant Orders</span>
+          </Link>
+        </SheetDescription>
+
+        <SheetFooter className="flex flex-col gap-2">
+          <div className="flex flex-row items-center gap-2">
+            <Avatar>
+              <AvatarImage />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+            <h1 className="font-bold">Eat&Dine</h1>
+          </div>
+          <SheetClose asChild>
+            <Button
+              type="submit"
+              className="w-full bg-orange hover:bg-hoverOrange"
+            >
+              Logout
+            </Button>
+          </SheetClose>
+        </SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 };
