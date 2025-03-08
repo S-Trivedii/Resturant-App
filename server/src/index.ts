7d57345dc2e7d83c13c6707ea5c9c44bcd9d2dd5
Shubhanshu Trivedi <itsshubhanshu03@gmail.com>
2025-03-08 22:18:07 +0530

    Connect db successfully

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./db/connectDB";
 dotenv.config();
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(() => {
+  connectDB();
   console.log(`Listening or port ${PORT}`);
 });
